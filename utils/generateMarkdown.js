392e3825dc2e5bcc74294367a5f9e6430cc2a837
ShaliniS FEBootCamper <118174874+ShaliniSunkuru@users.noreply.github.com>
2024-02-16 14:58:46 +0000

    fixed link in TOC

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,7 +10,7 @@ ${data.description}
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Contibuting](#contribution)
+- [Contibuting](#contributing)
 - [Tests](#tests)
 - [License](#license)
 - [Questions](#questions)
